d197d4ea2a89fb9f521a18f836707ba73f1eb0d7
Vincent Doré-Millet <vincent.dore-millet@unity3d.com>
2023-09-14 09:51:27 -0400

    Added simple landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let slackLink = "https://join.slack.com/t/nrqcworkspace/shared_invite/zt-23gpwc480-vp4a0ChQYvgnz1RC3e8Rwg";
+</script>
+
+<style>
+    .container
+    {
+        font-family: 'Fira Code', monospace;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+
+    .logo
+    {
+        width: 512px;
+        margin-bottom: 20px;
+    }
+
+    .description
+    {
+        text-align: center;
+        font-size: 20px;
+    }
+</style>
+
+<div class="container">
+    <img class="logo" src="logo.png" alt="Numérique Responsable Québec" />
+    <p class="description">
+        Rejoindre la conversation sur <a href={slackLink}>Slack</a>
+    </p>
+</div>
